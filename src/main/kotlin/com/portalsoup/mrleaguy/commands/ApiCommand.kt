6678fde249291d05a4b75a161cbe586a9cfdc1e8
julian <portalsoup@gmail.com>
2019-12-16 23:10:16 -0800

    maybe exception is happening?

--- src/main/kotlin/com/portalsoup/mrleaguy/commands/ApiCommand.kt
+++ src/main/kotlin/com/portalsoup/mrleaguy/commands/ApiCommand.kt
@@ -11,36 +11,44 @@ abstract class ApiCommand : AbstractCommand() {
     private val apiClient = OkHttpClient()
 
     fun makeRequest(url: String): JSONObject {
-        val request = Request.Builder()
-            .url(url)
-            .build()
+        try {
+            val request = Request.Builder()
+                .url(url)
+                .build()
 
-        var done = false
-        var response = ""
+            var done = false
+            var response = ""
 
-        apiClient.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                done = true
+            apiClient.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    done = true
+                }
+
+                override fun onResponse(call: Call, r: Response) {
+                    response = r.body()!!.string()
+                    done = true
+                }
+            })
+
+            print("Waiting ")
+            while (!done) {
+                print(".")
+                Thread.sleep(500)
+            }
+            println("")
+
+            if (response.isEmpty() || response == "null") {
+                println("Failed: response=${response}")
+                throw NoResultsFoundException()
             }
 
-            override fun onResponse(call: Call, r: Response) {
-                response = r.body()!!.string()
-                done = true
-            }
-        })
-
-        print("Waiting ")
-        while (!done) {
-            print(".")
-            Thread.sleep(500)
+            println("Returning")
+            return JSONObject(response)
+        } catch (e: RuntimeException) {
+            println("FAILED!" + e.message)
+            e.printStackTrace()
+            throw e
         }
-
-        if (response.isEmpty() || response == "null") {
-            println("Failed: response=${response}")
-            throw NoResultsFoundException()
-        }
-
-        return JSONObject(response)
     }
     protected class NoResultsFoundException: RuntimeException()
 
