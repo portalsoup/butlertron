301f42223b2c3448b6dc453f392c270a9267b00b
julian <portalsoup@gmail.com>
2020-01-23 22:09:32 -0800

    Gravy's motorway 1.0

---
+++ src/main/kotlin/com/portalsoup/mrbutlertron/commands/GravysMotorway.kt
@@ -0,0 +1,27 @@
+package com.portalsoup.mrbutlertron.commands
+
+import com.portalsoup.discordbot.core.command.GuildMessageReceivedCommand
+import com.portalsoup.discordbot.core.command.command
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+
+class GravysMotorway : GuildMessageReceivedCommand<GuildMessageReceivedEvent>(
+
+    command {
+        preconditions {
+            predicate {
+                it.message.contentRaw
+                    .trim()
+                    .toLowerCase()
+                    .equals("i've got a meal on the motorway to the floater-way. you say?")
+            }
+        }
+
+        job {
+            run {
+                it.channel
+                    .sendMessage("Don't rush the flush, Daddio! Listen to the ass! Pass the gas! Listen to the ass! Pass the gas!")
+                    .queue()
+            }
+        }
+    }
+)
